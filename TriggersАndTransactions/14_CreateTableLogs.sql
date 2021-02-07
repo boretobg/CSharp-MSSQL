837a5e0a4914cef3a89eff514481f06d445accdc
Bobby Stefanov <boretobg@gmail.com>
2021-02-07 15:16:29 +0200

    14.CreateTableLogs Done

--- TriggersАndTransactions/14_CreateTableLogs.sql
+++ TriggersАndTransactions/14_CreateTableLogs.sql
@@ -1,3 +1,7 @@
+
+/*Create Table Logs 
+Create a table – Logs (LogId, AccountId, OldSum, NewSum). Add a trigger to the Accounts table that enters a new entry into the Logs table every time the sum on an account changes. Submit only the query that creates the trigger.*/
+
 CREATE TRIGGER tr_AddLogsAfterAccountUpdate
 ON Accounts FOR UPDATE
 AS
