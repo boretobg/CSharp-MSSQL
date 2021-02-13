16693e6b13b77956cfa139420491893c249a1828
Bobby Stefanov <boretobg@gmail.com>
2021-02-13 13:44:10 +0200

    Exam done 100/100

--- FinalExam13Feb/08_SingleFiles.sql
+++ FinalExam13Feb/08_SingleFiles.sql
@@ -1,9 +1,5 @@
-
---MY MIND JUST TURNED OFF idk what happened I could find a solution for an hours, while the rest of the exam I did IN 1 hour
-SELECT 
-	Id, Name, CONCAT(Size, 'KB')
-FROM Files 
-	WHERE Id IN (6, 12, 17, 18, 19, 23, 29, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 48, 49, 50)
-ORDER BY Id, Name, Size DESC
-
-
+SELECT f1.Id, f1.Name, CONCAT(Size, 'KB')
+	FROM Files f1
+	LEFT JOIN Files f2 ON f2.ParentId = f1.Id
+WHERE f2.Id IS NULL
+ORDER BY f1.Id, f1.Name, f1.Size
